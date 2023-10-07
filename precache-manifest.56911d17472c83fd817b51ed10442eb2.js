9383929872f8b77aaf175be683a8662231ef77ad
ismaelfari1996 <abel_fari@hotmail.com>
2023-10-07 11:03:18 -0500

    Updates

---
+++ precache-manifest.56911d17472c83fd817b51ed10442eb2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6c94464c67d26abd9f070831b735f5",
+    "url": "/ismael-portafolio/index.html"
+  },
+  {
+    "revision": "45e6df223de8d5d234a0",
+    "url": "/ismael-portafolio/static/css/main.4fceaf0e.chunk.css"
+  },
+  {
+    "revision": "657f9401486320d4d7dd",
+    "url": "/ismael-portafolio/static/js/2.8b523211.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/ismael-portafolio/static/js/2.8b523211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0340fe890528dcfd1ec",
+    "url": "/ismael-portafolio/static/js/3.b6f79ded.chunk.js"
+  },
+  {
+    "revision": "45e6df223de8d5d234a0",
+    "url": "/ismael-portafolio/static/js/main.e12b77b6.chunk.js"
+  },
+  {
+    "revision": "e7428c0b6e3c3c25c366",
+    "url": "/ismael-portafolio/static/js/runtime-main.5477e1cc.js"
+  },
+  {
+    "revision": "484816988515fa0839aaf0d23ef9d10c",
+    "url": "/ismael-portafolio/static/media/cover.48481698.webp"
+  },
+  {
+    "revision": "029f0deb716e12c62af7ed9f3635915b",
+    "url": "/ismael-portafolio/static/media/cover1.029f0deb.jpg"
+  },
+  {
+    "revision": "e129d18e7a383311201a5c8c37b7f0a7",
+    "url": "/ismael-portafolio/static/media/install.e129d18e.png"
+  },
+  {
+    "revision": "30bb6d46216069e1cd3832cd805a7dee",
+    "url": "/ismael-portafolio/static/media/profile.30bb6d46.png"
+  },
+  {
+    "revision": "435199afe06da6af69c9d4cda4192adb",
+    "url": "/ismael-portafolio/static/media/setting.435199af.png"
+  }
+]);
